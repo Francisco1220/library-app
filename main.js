8b682f86816af8e221f3a93c826c928f0003819f
Francisco <franciscocolombocouste@gmail.com>
2024-09-16 14:16:57 +0200

    Replace constructor function with class

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 /* Takes existing myLibrary array and adds new additions(objects) as part of array */
